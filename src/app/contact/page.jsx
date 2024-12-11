923987142bd1969cd2819817eebd234890dbd1ac
Hanyun1 <hanyzhang@student.unimelb.edu.au>
2024-12-11 22:07:59 +1100

    initialise contact page

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <section className="container bg-gray-100 pt-10 items-center justify-between ">
+        
+        
+        <div className="container p-4 bg-gray-100 basis-3/5">
+        <div className="text-2xl font-bold ">Contact me</div>
+        {/* <div className="flex flex-col md:flex-row gap-2">
+          <SideWidgetBar
+            onSelect={setSelectedContent}
+            selectedContent={selectedContent}
+            className="flex-3 basis-1/5"
+          />
+          <div className="flex p-4 basis-4/5">{renderContent()}</div>
+        </div> */}
+      </div>
+        
+        </section>
+  )
+}
+
+export default Contact
