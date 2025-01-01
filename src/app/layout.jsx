09bf50bdacaab8db941cd4adeee1ae493dd9cb7c
Hanyun1 <hanyzhang@student.unimelb.edu.au>
2025-01-01 15:25:23 +1100

    add load indicator

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import NextTopLoader from "nextjs-toploader";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={jetbrainsMono.variable} suppressHydrationWarning={true}>
         <Header />
+        <NextTopLoader />
         {children}
       </body>
     </html>
